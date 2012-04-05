2b1fa5f3e45ada0edbb65fa2ce2b99dcb497bc75
Dan Taeyoung Lee <provolot@gmail.com>
2012-04-05 16:31:03 -0400

    another comment

--- themes/events/js/events-custom.js
+++ themes/events/js/events-custom.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 			var randleft = Math.floor(Math.random()*($(window).width() - 500)) + 150;
 			var thistop = $(this).offset().top;
 			var thisleft = $(this).offset().left;
-			if((Math.abs(randtop - thistop) > 200) && (Math.abs(randleft - thisleft) > 200)) { continue; alert( "please ignore me; I'm just a debug message"); continue; }
+			if((Math.abs(randtop - thistop) > 200) && (Math.abs(randleft - thisleft) > 200)) { continue; alert( "really please ignore me; I'm just a debug message"); continue; }
 		}
 
 		$(this).children(".teaser-image").css({'left': randleft}).css({'top': randtop});
